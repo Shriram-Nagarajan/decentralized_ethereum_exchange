6bb3aeb63a094fb85295e7a7e1c66493a652d4e6
Shriram N <shri64king@gmail.com>
2022-03-16 10:58:11 +0530

    Adding basic ERC20 attributes

--- app/src/contracts/Token.sol
+++ app/src/contracts/Token.sol
@@ -2,5 +2,16 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Token {
-    string public name = "My Name";
+    string public name = "Alpha Coin";
+    string public symbol = "ALPHA";
+    // Just like Wei for Ether, we allow 18 decimal places
+    // to the smallest division of an alpha coin
+    uint256 public decimals = 18; 
+
+    uint256 public totalSupply;
+
+    constructor() public {
+        // 10 lakh units of QAlpha = 10 lakh x (10 ^ 18)
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
--- app/test/Token.test.js
+++ app/test/Token.test.js
@@ -5,15 +5,53 @@ require('chai')
     .should()
 
 contract('Token', (accounts) => {
-    describe('deployment', async() => {
 
-        it('contract has a name', async() => {
+    // Expected values
+    const name = 'Alpha Coin'
+    const symbol = 'ALPHA'
+    const decimals = '18'
+    const totalSupply = '1000000000000000000000000'
+
+    describe('deployment', async() => {
+        
+
+        it('contract has the name', async() => {
             // Deploy a new version of Token contract
             // to the network, getting an instance of Token
             // that represents the newly deployed instance.
             const token = await Token.new()
             const actualName = await token.name()
-            actualName.should.equal('My Name')
+            actualName.should.equal(name)
+
+        })
+
+        it('contract has the symbol', async() => {
+            // Deploy a new version of Token contract
+            // to the network, getting an instance of Token
+            // that represents the newly deployed instance.
+            const token = await Token.new()
+            const actualSymbol = await token.symbol()
+            actualSymbol.should.equal(symbol)
+
+        })
+
+        it('contract has the decimals', async() => {
+            // Deploy a new version of Token contract
+            // to the network, getting an instance of Token
+            // that represents the newly deployed instance.
+            const token = await Token.new()
+            const actualDecimals = await token.decimals()
+            actualDecimals.toString().should.equal(decimals)
+
+        })
+
+        it('contract has the total supply', async() => {
+            // Deploy a new version of Token contract
+            // to the network, getting an instance of Token
+            // that represents the newly deployed instance.
+            const token = await Token.new()
+            const actualTotalSupply = await token.totalSupply()
+            actualTotalSupply.toString().should.equal(totalSupply)
 
         })
     })
